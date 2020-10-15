ec2b49ee57141cd1a4b2e69eb050660f68d7299e
zys <zoysoo@naver.com>
2020-10-15 10:20:09 +0900

    휴대폰번호 가리기

--- project1/src/project1/Cocomanmo_5.java
+++ project1/src/project1/Cocomanmo_5.java
@@ -6,7 +6,7 @@ public class Cocomanmo_5 {
 	        String answer = "";
 	        int len = phone_number.length();
 	        StringBuffer sb = new StringBuffer();
-	        
+	       
 	            for(int i=0;i<len-4;i++) {
 	                sb.append("*");
 	            }
