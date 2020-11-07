6ec6b80040aac30ebab3786ae3e389f8e08d1cd2
zys <zoysoo@naver.com>
2020-11-07 16:14:19 +0900

    평균구하기

---
+++ project1/src/project1/Cocomanmo14.java
@@ -0,0 +1,17 @@
+package project1;
+
+public class Cocomanmo14 {
+	class Solution {
+	    public double solution(int[] arr) {
+	        double answer = 0;
+	        double sum=0; 
+	        for(int i=0; i<arr.length; i++){
+	            sum += arr[i];
+	        }
+	        answer = sum/arr.length;
+	        
+	        return answer;
+	    }
+	}
+
+}
